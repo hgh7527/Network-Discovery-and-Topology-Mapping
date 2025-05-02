c0d90ecfafdeffd7676c188bf213db29d6230bb2
James Oakes <jto0706@utulsa.edu>
2025-05-02 01:38:47 -0500

    created a starter script

---
+++ vuln-process-discovery.py
@@ -0,0 +1,22 @@
+#This script automates Vulnerability and Rogue Process Discovery
+#
+
+import sys
+import os
+import open
+import psutil
+
+def CheckOpenPorts(): #good to have but optional
+    return
+
+def VulnerabilityScan():
+    return
+
+def ProcessDiscovery():
+    return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
